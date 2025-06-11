8bebfa6ab90a37e6c5967d8b762476592c327f9e
lihao <lih@htcxfund.com>
2025-06-11 14:42:33 +0800

    v0.0.1

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+from setuptools import setup, find_packages
+
+def read_requirements():
+    with open('requirements.txt', 'r', encoding='utf8') as f:
+        return f.readlines()
+
+setup(
+    name="schd-server",
+    version="0.0.1",
+    url="https://github.com/kevenli/schd-server",
+    packages=find_packages(exclude=('tests', 'tests.*')),
+    install_requires=read_requirements(),
+    license="ApacheV2",
+    license_files="LICENSE",
+)
